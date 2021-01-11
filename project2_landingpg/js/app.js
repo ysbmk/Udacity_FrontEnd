65f2f897434a4b1a58aa9b4a35f962d9525f020d
ysbmk <ysbmk.tango@gmail.com>
2021-01-11 20:42:22 +0900

    Added response when scrolling for topnav

--- project2_landingpg/js/app.js
+++ project2_landingpg/js/app.js
@@ -22,39 +22,40 @@ const navbarList    = document.querySelector("#navbar__list");
 const section1      = document.getElementById("section1")
 const section2      = document.getElementById("section2")
 const section3      = document.getElementById("section3")
-const items         = ["section1", "section2", "section3"];
+const section4      = document.getElementById("section4")
+const items         = ["section1", "section2", "section3", "section4"];
 let positions       = {}
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function calcPositions() {
-    let pageY   = window.innerHeight ;
+// function calcPositions() {
+//     let pageY   = window.innerHeight ;
 
-    console.log(`pageY:${pageY}`);
-    let bottom1 = section1.getBoundingClientRect().bottom;
-    let top2    = section2.getBoundingClientRect().top;
-    let bottom2 = section2.getBoundingClientRect().bottom;
-    let top3    = section3.getBoundingClientRect().top;
-    let bottom3 = section3.getBoundingClientRect().bottom;
+//     console.log(`pageY:${pageY}`);
+//     let bottom1 = section1.getBoundingClientRect().bottom;
+//     let top2    = section2.getBoundingClientRect().top;
+//     let bottom2 = section2.getBoundingClientRect().bottom;
+//     let top3    = section3.getBoundingClientRect().top;
+//     let bottom3 = section3.getBoundingClientRect().bottom;
 
-    if (pageY <bottom1){
-        section1.classList.add("active");
-    } else {
-        section1.classList.remove("active");
-    };
-    if (pageY>=top2 && pageY <bottom2){
-        section2.classList.add("active");
-    } else {
-        section2.classList.remove("active");
-    };
-    if (pageY>=top3 && pageY <bottom3){
-        section3.classList.add("active");
-    } else {
-        section3.classList.remove("active");
-    };
-}
+//     if (pageY <bottom1){
+//         section1.classList.add("active");
+//     } else {
+//         section1.classList.remove("active");
+//     };
+//     if (pageY>=top2 && pageY <bottom2){
+//         section2.classList.add("active");
+//     } else {
+//         section2.classList.remove("active");
+//     };
+//     if (pageY>=top3 && pageY <bottom3){
+//         section3.classList.add("active");
+//     } else {
+//         section3.classList.remove("active");
+//     };
+// }
 
 /**
  * End Helper Functions
@@ -66,6 +67,7 @@ function calcPositions() {
 // Link of each item is a placeholder.
 for (let item of items){
     const navItem       = document.createElement("li");
+    navItem.id          = `link__${item}`;
     const targetSection =  document.getElementById(`${item}`);
     navItem.innerHTML   = `<a class="menu__link">${item.toUpperCase()}</a>`;
     navItem.addEventListener('click', function (evt) {   
@@ -82,26 +84,37 @@ document.addEventListener("scroll", function () {
 
     // console.log(`pageY:${pageY}`);
     let bottom1 = section1.getBoundingClientRect().bottom;
-    let top2    = section2.getBoundingClientRect().top;
     let bottom2 = section2.getBoundingClientRect().bottom;
-    let top3    = section3.getBoundingClientRect().top;
     let bottom3 = section3.getBoundingClientRect().bottom;
 
     if (200<bottom1){
         section1.classList.add("active");
+        document.getElementById("link__section1").classList.add("active");
     } else {
         section1.classList.remove("active");
+        document.getElementById("link__section1").classList.remove("active");
     };
 
-    if (200>bottom1 ){
+    if (200>bottom1 &&200<bottom2){
         section2.classList.add("active");
+        document.getElementById("link__section2").classList.add("active");
     } else {
         section2.classList.remove("active");
+        document.getElementById("link__section2").classList.remove("active");
     };
-    if (200>bottom2 ){
+    if (200>bottom2 && 200<bottom3){
         section3.classList.add("active");
+        document.getElementById("link__section3").classList.add("active");
     } else {
         section3.classList.remove("active");
+        document.getElementById("link__section3").classList.remove("active");
+    };
+    if (200>bottom3 ){
+        section4.classList.add("active");
+        document.getElementById("link__section4").classList.add("active");
+    } else {
+        section4.classList.remove("active");
+        document.getElementById("link__section4").classList.remove("active");
     };
 }
 )
